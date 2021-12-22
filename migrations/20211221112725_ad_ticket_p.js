f8a38c8aa5667bddf8535c5d92dcdef5e3f0c21f
Hiruy <91972582+hiruy69@users.noreply.github.com>
2021-12-22 14:24:59 +0300

    Update 20211221112725_ad_ticket_p.js

--- migrations/20211221112725_ad_ticket_p.js
+++ migrations/20211221112725_ad_ticket_p.js
@@ -1,7 +1,16 @@
 const { v4: uuidv4 } = require('uuid');
 exports.up = function(knex) {
-    return knex.schema.table('tickets', function(t) {
-        t.uuid('id').unique().primary().defaultTo(uuidv4())
+    return knex.schema
+    .createTable('users', function (table) {
+      table.increments('id').primary();
+      table.string('name', 255)
+      table.string('phone', 255);
+      table.string('username', 255).unique().notNullable();
+      table.string('password', 255);
+      table.string('address', 255).notNullable();
+      table.string('shopname', 255).notNullable();
+      table.string('price', 255).notNullable().defaultTo("13");
+      //table.timestamps().notNullable().defaultTo(knex.fn.now());
     });
 };
 
