fee1c88dc188f09996ee21c8586974628d1f0cac
Hiruy <91972582+hiruy69@users.noreply.github.com>
2021-12-22 11:27:30 +0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,6 @@
 // Update with your config settings.
 
 module.exports = {
-
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      directory: './migrations',
-      tableName: 'knex_migrations'
-    }
-  }
-
+  client: 'pg',
+  connection: process.env.DATABASE_URL
 };
