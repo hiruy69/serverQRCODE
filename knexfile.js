1dae45c13e3ebaad9fcb2c377e9fd21eb7b55a00
Hiruy <91972582+hiruy69@users.noreply.github.com>
2021-12-22 10:53:09 +0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,27 +9,11 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './migrations',
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
+      directory: './migrations',
       tableName: 'knex_migrations'
     }
   }
