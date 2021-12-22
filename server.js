5776721444f90029d6f7e59d74541b1f3c74a95c
Hiruy <91972582+hiruy69@users.noreply.github.com>
2021-12-22 10:44:46 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const  express =  require('express');
 const bodyParser =  require( 'body-parser');
-const  db =  require( './repositories/users'); 
-require("dotenv").config();
+//const  db =  require( './repositories/users'); 
+//require("dotenv").config();
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const cors = require('cors')
@@ -12,7 +12,7 @@ const cors = require('cors')
 const port = process.env.PORT || 3000;
 const app = express();
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 app.use(cors())
 app.listen(port, () => console.log(`Node-JWT-SQLite-Starer is listening on port ${port}!`));
 app.use(bodyParser.json());
